a22071aa626bd3d8a51fe939821f214865fae8a0
Tracey <tenthconcept@gmail.com>
2023-08-08 19:58:46 -0700

    snapshot application.c

--- m2x.js
+++ m2x.js
@@ -1,1 +1,8 @@
-const vte = imports.vte;
+// m2x.js
+//
+
+import Vte from "gi://Vte";
+import GLib from "gi://GLib";
+import Gio from "gi://Gio";
+import Gdk from "gi://Gdk";
+import Adw from "gi://Adw";
