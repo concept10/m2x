c367970f94c95862185ecdbd4ccd1a3cefb7e621
Tracey <tenthconcept@gmail.com>
2025-03-03 22:33:15 -0500

    Update memory-map.c

--- src/research/memory-map.c
+++ src/research/memory-map.c
@@ -1,5 +1,21 @@
 #include <glib-object.h>
 
+/**
+ * MemoryMap:
+ * @parent_instance: The parent instance.
+ * @sample_ram: Sample RAM with a size of 0x8000.
+ * @os_ram: OS RAM with a size of 0x4000.
+ * @seq_expander_ram: Sequential expander RAM with a size of 0x2000.
+ * @io_ports: IO ports with a size of 0x1000.
+ * @acia_control_port: ACIA control port.
+ * @via_io_register_b: VIA IO register B.
+ * @vcf_dac: VCF DAC.
+ * @floppy_disk_controller: Floppy disk controller.
+ * @doc_5503: DOC 5503.
+ * @boot_rom: Boot ROM with a size of 0x1000.
+ *
+ * This struct represents the memory map for the Mirage system.
+ */
 typedef struct {
     GObject parent_instance;
 
@@ -15,12 +31,25 @@ typedef struct {
     guint16 boot_rom[0x1000];
 } MemoryMap;
 
+/**
+ * MemoryMapClass:
+ * @parent_class: The parent class.
+ *
+ * This struct represents the class for the MemoryMap.
+ */
 typedef struct {
     GObjectClass parent_class;
 } MemoryMapClass;
 
 G_DEFINE_TYPE(MemoryMap, memory_map, G_TYPE_OBJECT);
 
+/**
+ * memory_map_init:
+ * @self: A pointer to the MemoryMap instance.
+ *
+ * Initializes the memory regions for the MemoryMap instance. This function sets
+ * all memory regions to zero.
+ */
 static void memory_map_init(MemoryMap *self) {
     // Initialize memory regions
     memset(self->sample_ram, 0, sizeof(self->sample_ram));
@@ -35,17 +64,35 @@ static void memory_map_init(MemoryMap *self) {
     memset(self->boot_rom, 0, sizeof(self->boot_rom));
 }
 
+/**
+ * memory_map_class_init:
+ * @klass: A pointer to the MemoryMapClass.
+ *
+ * Initializes the MemoryMap class. This function is currently empty, but it
+ * could be used to set up class-specific properties or signals in the future.
+ */
 static void memory_map_class_init(MemoryMapClass *klass) {
     // Class initialization
 }
 
+/**
+ * main:
+ *
+ * The main function for the program. It creates a new MemoryMap instance,
+ * accesses some memory regions and ports, and then unrefs the MemoryMap
+ * instance before exiting.
+ *
+ * Returns: 0 on successful execution.
+ */
 int main() {
+    // Create a new MemoryMap instance
     MemoryMap *memory_map = g_object_new(memory_map_get_type(), NULL);
 
     // Access memory regions and ports
     memory_map->acia_control_port = 0xA5;
     memory_map->sample_ram[0x1234] = 0x5678;
 
+    // Unref the MemoryMap instance
     g_object_unref(memory_map);
     return 0;
 }
